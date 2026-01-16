f1572c251400fc8c5283e9a57240eb8ca3fa39a4
fakhreddinelargou <largofakhrddin@gmail.com>
2026-01-16 16:01:43 +0100

    feat: add OTP verification for user authentication

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,9 +1,11 @@
 <?php
-class Controller{
-protected function view($path , $data = []){
-    extract($data);
-    $url = "../app/views/" . $path . ".php";
-    require  $url ;
-}
+class Controller
+{
+    protected function view($path, $data = [])
+    {
+        extract($data);
+        $url = "../app/views/" . $path . ".php";
+        require $url;
+    }
 }
 ?>
